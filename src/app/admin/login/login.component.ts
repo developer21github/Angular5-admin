5e552eecd90850e8f0f265f79dc52bd7422771c1
riya <riya@johnsnowlabs.com>
2017-12-28 11:59:47 +0530

    login added

--- src/app/admin/login/login.component.ts
+++ src/app/admin/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  public href: any = '';
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    this.href = this.router.url;
+    console.log(this.href);
   }
 
 }
